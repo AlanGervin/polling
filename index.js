3cb1404d2be0124378ae6d892f92116924e1d0ba
Alan Gervin <alan.gervin@gmail.com>
2021-01-16 20:12:38 -0800

    updated

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+
 /* Config */
 const twitchTvHandle = "swervinmaximus";
-
-/* DOM */
-const container = document.querySelector(".poll");
-
-container.style.opacity = 1;
 
 
 var yes = 0;
 var no = 0;
+
+
+/* DOM */
+const yesContainer = document.querySelector(".yesContainer");
+const noContainer = document.querySelector(".noContainer");
+
+yesContainer.style.opacity = 1;
+yesContainer.style.opacity = 1;
+yesContainer.innerHTML = yes;
+noContainer.innerHTML = no;
+
+
 
 ComfyJS.Init(twitchTvHandle);
 ComfyJS.onChat = (user, message, flags, self, extra) => {
@@ -16,13 +24,36 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
     if (message === "prediction") {
         yes = 0;
         no = 0;
-    }
-    if (message === '1') {
+        yesContainer.innerHTML = yes;
+        noContainer.innerHTML = no;
+    } else if (message === '1') {
+ 
         yes++;
+        yesContainer.innerHTML = yes;
+
+
+
+        const queryResults1 = UserVote.countDocuments({"userId": extra.displayName}, (err, count) => {
+            if (count == 0) {
+              
+                //UserVote.create({"userId": extra.displayName, "vote": "yes"});
+     
+            }
+        }); 
+        
+      
+    } else if (message === '0') {
+        no++;
+        noContainer.innerHTML = no;
+        const queryResults = UserVote.countDocuments({"userId": extra.displayName}, (err, count) => {
+            if (count == 0) {
+                //UserVote.create({"userId": extra.displayName, "vote": "no"});
+
+            }
+        });        
+      
     }
+
 };
   
-
-
-container.innerHTML = yes;
 
